65202a5be116876006b9845fbd44930c3937fde2
Sam Thonis <samthonis@gmail.com>
2021-01-31 13:12:07 -0500

    Add test for HTMLOuput.js that looks for multiple lines output

--- src/spec/HTMLOutput.spec.js
+++ src/spec/HTMLOutput.spec.js
@@ -10,4 +10,14 @@ describe('HTMLOutput', () => {
         render(<HTMLOutput showNotes={showNotes} />)
         screen.getByText('<li><a class="jump-point button underline" href="#1:14">1:14</a> - <a href="https://example.com">the best part</a></li>')
     })
+
+    it("renders multiple LIs for multiple show notes", () => {
+        const showNotes = [
+            { id: 1, timestamp: '1:14', url: 'https://example.com', linktext: 'the best part'},
+            { id: 2, timestamp: '3:14', url: 'https://example2.com', linktext: 'the second best part'}
+        ]
+        render(<HTMLOutput showNotes={showNotes} />)
+        screen.getByText('<li><a class="jump-point button underline" href="#1:14">1:14</a> - <a href="https://example.com">the best part</a></li>')
+        screen.getByText('<li><a class="jump-point button underline" href="#3:14">3:14</a> - <a href="https://example2.com">the second best part</a></li>')
+    })
 })
